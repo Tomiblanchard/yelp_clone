eaa1dd4d1cd1e59fb9925b84def706c7e86fc836
Tomiblanchard <tomiblanchard@gmail.com>
2015-05-06 15:19:05 +0100

    can read restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,17 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
