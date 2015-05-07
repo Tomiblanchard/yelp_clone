64ad84f47620de957d4786d4e916ce50d646a995
Tomiblanchard <tomiblanchard@gmail.com>
2015-05-07 10:41:39 +0100

    fixed nilclass error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
 
-  def index
-    @restaurants = Restaurant.all
-  end
-
   def new
     @restaurant = Restaurant.new
+  end
+
+  def index
+    @restaurants = Restaurant.all
   end
 
   def create
