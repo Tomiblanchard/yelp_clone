8419628495bfaadd75de7dfecc2f8649182bb2c1
Tomiblanchard <tomiblanchard@gmail.com>
2015-05-07 17:59:31 +0100

    can login with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: "restaurants#index"
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :restaurants do
+    resources :reviews
+
+    devise_scope :user do
+      get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,11 +18,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  resources :restaurants do
-    resources :reviews
-  end
 
-  root to: "restaurants#index"
+
+
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
